042ae7f73772f13d465a818ad2cc657c1fa923d4
Joseph Emmanuel <117820245+Fmsticks2@users.noreply.github.com>
2025-11-30 14:02:48 +0100

    deps installed

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    const port = Number(process.env.WEB_PORT ?? env.WEB_PORT ?? 5173);
+    return {
+        server: {
+            port,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
+        }
+    };
+});
